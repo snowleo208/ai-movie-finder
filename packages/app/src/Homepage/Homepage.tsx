517c0ef8e3cdcc7af2f768b3a1ccfe3bf28e2e47
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2024-10-15 00:05:04 +0100

    feat: prompt bar

--- packages/app/src/Homepage/Homepage.tsx
+++ packages/app/src/Homepage/Homepage.tsx
@@ -1,15 +1,12 @@
 import { Flex, Text, Button } from "@radix-ui/themes";
-import { useAiModal } from "./useAiModel";
+// import { fetchResponse } from "./useAiModel.ts";
+import { PromptBar } from "./PromptBar.client.tsx";
 
 export const Homepage = async () => {
-  const { fetchResponse } = useAiModal();
-
-  await fetchResponse();
-
   return (
     <Flex direction="column" gap="2">
       <Text>Hello!</Text>
-      <Button>Let's go</Button>
+      <PromptBar />
     </Flex>
   );
 };
